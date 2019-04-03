ced155ccaa73b18f738aa539aad43a59fcbab37c
lilin <lin.li@dyuanzi.com>
2019-04-03 15:30:39 +0800

    refactor:二维码扫描结果全部使用TextResultHandler去处理

--- zxinglibrary/src/main/java/com/dyz/pumei/zxinglibrary/result/ResultHandlerFactory.java
+++ zxinglibrary/src/main/java/com/dyz/pumei/zxinglibrary/result/ResultHandlerFactory.java
@@ -17,10 +17,9 @@
 package com.dyz.pumei.zxinglibrary.result;
 
 import com.dyz.pumei.zxinglibrary.CaptureActivity;
-import com.dyz.pumei.zxinglibrary.result.ResultHandler;
 import com.google.zxing.Result;
 import com.google.zxing.client.result.ParsedResult;
-import com.google.zxing.client.result.ResultParser;
+import com.google.zxing.client.result.TextParsedResult;
 
 /**
  * Manufactures Android-specific handlers based on the barcode content's type.
@@ -28,38 +27,16 @@ import com.google.zxing.client.result.ResultParser;
  * @author dswitkin@google.com (Daniel Switkin)
  */
 public final class ResultHandlerFactory {
-  private ResultHandlerFactory() {
-  }
+    private ResultHandlerFactory() {
+    }
 
-  public static ResultHandler makeResultHandler(CaptureActivity activity, Result rawResult) {
-    ParsedResult result = parseResult(rawResult);
-    switch (result.getType()) {
-//      case ADDRESSBOOK:
-//        return new AddressBookResultHandler(activity, result);
-//      case EMAIL_ADDRESS:
-//        return new EmailAddressResultHandler(activity, result);
-//      case PRODUCT:
-//        return new ProductResultHandler(activity, result, rawResult);
-//      case URI:
-//        return new URIResultHandler(activity, result);
-//      case WIFI:
-//        return new WifiResultHandler(activity, result);
-//      case GEO:
-//        return new GeoResultHandler(activity, result);
-//      case TEL:
-//        return new TelResultHandler(activity, result);
-//      case SMS:
-//        return new SMSResultHandler(activity, result);
-//      case CALENDAR:
-//        return new CalendarResultHandler(activity, result);
-//      case ISBN:
-//        return new ISBNResultHandler(activity, result, rawResult);
-      default:
+    public static ResultHandler makeResultHandler(CaptureActivity activity, Result rawResult) {
+        ParsedResult result = parseResult(rawResult);
         return new TextResultHandler(activity, result, rawResult);
     }
-  }
 
-  private static ParsedResult parseResult(Result rawResult) {
-    return ResultParser.parseResult(rawResult);
-  }
+    private static ParsedResult parseResult(Result rawResult) {
+//    return ResultParser.parseResult(rawResult);
+        return new TextParsedResult(rawResult.getText(), null);
+    }
 }
